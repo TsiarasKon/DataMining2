21c85d01383f725bfd6e2037e30dce29f46f5a50
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2018-05-26 16:04:19 +0300

    LCSS Implementation
    Implemented LCSS with Dynamic Programming

---
+++ LCSS.py
@@ -0,0 +1,29 @@
+""" LCSS Implementation """
+# X, Y are the two trajectories
+# C is the matrix storing all matching points
+
+
+def LCSS(X, Y, matching_funct):
+    m = len(X)
+    n = len(Y)
+    # (m+1) x (n+1) matrix
+    C = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if matching_funct(X[i-1], Y[j-1]):
+                C[i][j] = C[i-1][j-1] + 1
+            else:
+                C[i][j] = max(C[i][j-1], C[i-1][j])
+    return C              # returns the whole matrix so that findSolution can be used on it
+
+
+def findSolution(C, X, Y, i, j, matching_funct):     # first call should be with i == len(X) and j == len(Y)
+    if i == 0 or j == 0:
+        return []
+    elif matching_funct(X[i-1], Y[j-1]):
+        return findSolution(C, X, Y, i-1, j-1).append(X[i-1])
+    else:
+        if C[i][j-1] > C[i-1][j]:
+            return findSolution(C, X, Y, i, j-1)
+        else:
+            return findSolution(C, X, Y, i-1, j)
